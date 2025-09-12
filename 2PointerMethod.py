af753277bd382a39c7f702c071fd2139d44dbd30
Alt-Chaitanya <chaitanyasutar6174@gmail.com>
2025-09-12 12:13:25 +0530

    Added 2poimter method

---
+++ 2PointerMethod.py
@@ -0,0 +1,28 @@
+#Two pointer method
+arr=[4, 5, 2, 99, 22, 88]
+arr.sort()
+#2,4,5,22,88,99
+print(arr)
+
+t=110
+
+L=0
+R=len(arr)-1
+
+
+while L<R: #if it was L>R it would've skipped the code 
+    sum=arr[L]+arr[R]
+
+    if sum==t:
+        print(f"Sum found at indexes: {L} and {R}.")
+        break
+        
+    elif sum<t:
+        L=L+1
+        
+    elif sum>t:
+        R=R-1
+        
+    else:
+        print("Sum not found")    
+                 
